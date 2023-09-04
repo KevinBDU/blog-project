5788a9e0c61313e9fab019e5aaa6f03accefe3ff
KevinBDU <127111084+KevinBDU@users.noreply.github.com>
2023-09-04 09:40:21 +0200

    modifs

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,23 @@
-const searchForm = document.getElementById('search-article')
-console.log(searchForm)
+document.addEventListener("DOMContentLoaded", function () {
+  const toggler = document.querySelector(".toggler");
+  const nav = document.querySelector("nav");
+
+  toggler.addEventListener("click", function () {
+    if (nav.style.display === "none" || nav.style.display === "") {
+      nav.style.display = "block";
+    } else {
+      nav.style.display = "none";
+    }
+  });
+});
+
+const searchForm = document.getElementById("search-article");
+console.log(searchForm);
 searchForm.addEventListener("submit", function (e) {
-    e.preventDefault()
-    e.stopPropagation()
-    const userSearch = this.elements["search-value"].value
-    const urlEncoded = encodeURI(`${BASE_URL}?userSearch=${userSearch}`)
-    console.log(urlEncoded)
-    window.location = urlEncoded
+  e.preventDefault();
+  e.stopPropagation();
+  const userSearch = this.elements["search-value"].value;
+  const urlEncoded = encodeURI(`${BASE_URL}?userSearch=${userSearch}`);
+  console.log(urlEncoded);
+  window.location = urlEncoded;
 });
